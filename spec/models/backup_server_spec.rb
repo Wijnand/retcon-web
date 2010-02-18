2fac2d59c11fa6bc0e9dfd6ebfff31dc1ad5765a
Wijnand Wiersma <wijnand@videre.net>
2010-02-18 18:27:46 +0100

    whitespace police

--- spec/models/backup_server_spec.rb
+++ spec/models/backup_server_spec.rb
@@ -9,34 +9,34 @@ describe BackupServer do
     b = Factory.build(:backup_server)
     b.valid?.should be true
   end
-  
+
   it "should not save when no hostname is given" do
     b = Factory.build(:backup_server, :hostname => nil)
     b.valid?.should be false
   end
-  
+
   it "should not save when no zpool is given" do
     b = Factory.build(:backup_server, :zpool => nil)    
     b.valid?.should be false
   end
-  
+
   it "should not save when no max_backups is given" do
     b = Factory.build(:backup_server, :max_backups => nil)
     b.valid?.should be false
   end
-  
+
   it "should have a to_s method" do
     b = Factory.build(:backup_server)
     b.to_s.should == b.hostname
   end
-  
+
   it "should have a way to call nanite jobs for a specific backup server" do
     b = Factory.build(:backup_server)
     b.should_receive(:nanites).and_return({"#{b.hostname}" => 'something'})
     Nanite.should_receive(:request).once.with("method", "arg", :target => "nanite-#{b.hostname}").and_yield("the result")
     b.send(:do_nanite, 'method', 'arg')
   end
-  
+
   it "should be able to query using nanite" do
     BackupServer.should_receive(:nanites).and_return({'backup2' => 'something', 'backup1' => 'something'})
     Nanite.should_receive(:request).once.with("command", "arg", :selector => :all).and_yield(
@@ -44,11 +44,11 @@ describe BackupServer do
     list = BackupServer.nanite_query("command", "arg")
     list['backup1'].should == "my result"
   end
-  
+
   it "should select valid backup servers for a given server" do
     backup1 = Factory(:backup_server)
     backup2 = Factory(:backup_server)
-    BackupServer.should_receive(:nanites).and_return({"#{backup1.hostname}" => 'something', 
+    BackupServer.should_receive(:nanites).and_return({"#{backup1.hostname}" => 'something',
                                                       "#{backup2.hostname}" => 'something'})
     Nanite.should_receive(:request).once.with("/info/in_subnet?", "localhost", :selector => :all).and_yield(
            {"#{backup1.hostname}" => true, "#{backup2.hostname}" => false})
@@ -58,7 +58,7 @@ describe BackupServer do
     available[0].in_subnet.should == true
     available[1].in_subnet.should == false
   end
-  
+
   it "should know if it's online" do
     backup1 = Factory(:backup_server)
     backup2 = Factory(:backup_server)
@@ -66,12 +66,12 @@ describe BackupServer do
     backup1.online?.should be true
     backup2.online?.should be false
   end
-  
+
   # very bad test since it actually calls the methods on Server instances
   it "should know which servers it should backup" do
     b = Factory(:backup_server)
     s1 = Factory(:server, :hostname => 'server1.example.com', :backup_server => b)
-    s2 = Factory(:server, :hostname => 'server2.example.com', :backup_server => b, 
+    s2 = Factory(:server, :hostname => 'server2.example.com', :backup_server => b,
                  :last_backup => Time.new - (3 * 3600), 
                  :last_started =>Time.new - ( 4 * 3600))
     s3 = Factory(:server, :hostname => 'server3.example.com', :backup_server => b)

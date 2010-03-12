381f775e3fc8764a21198423ea04a0d063347b28
Wijnand Wiersma <wijnand@videre.net>
2010-03-12 18:37:52 +0100

    The backup_server count cell was pretty useless

--- app/cells/backup_servers_cell.rb
+++ app/cells/backup_servers_cell.rb
@@ -1,11 +1,6 @@
 class BackupServersCell < Cell::Base
   helper :application
   
-  def count
-    @backup_servers = BackupServer.all
-    render
-  end
-  
   def status
     @backup_servers = BackupServer.all
     render

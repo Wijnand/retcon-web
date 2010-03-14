1bfa70c88f9d7b0103a7cebc20b92332b5a20882
Wijnand Wiersma <wijnand@kamahl.local>
2010-03-14 14:21:20 +0100

    Allow http basic auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.allow_http_basic_auth = true
+  end
+  
   attr_accessible :username, :password, :password_confirmation, :email
   has_many :roles, :through => :roles_users
   has_many :roles_users

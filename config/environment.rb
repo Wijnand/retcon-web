01ed654055b88f4a3b577193f5e118845bce948d
Wijnand Wiersma <wijnand@videre.net>
2010-03-18 22:24:19 +0100

    Don't require the nanite gem anymore

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,6 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "formtastic"
   config.gem "declarative_authorization"
-  config.gem "nanite"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

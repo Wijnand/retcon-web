fd8e298431af6bb738fc6131714841983b3ab233
Wijnand Wiersma <wijnand@videre.net>
2010-03-21 21:58:12 +0100

    Prevent duration lookups for nil

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -1,6 +1,7 @@
 module ServersHelper
   
   def display_backup_duration(job)
+    return 'Unknown' unless job
     return 'Not yet started' if job.status == 'queued'
     distance_of_time_in_words(job.created_at, job.updated_at )
   end

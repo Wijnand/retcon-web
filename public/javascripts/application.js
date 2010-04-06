b8b3d1145319c807118b7280cfc8e61de06445e3
Wijnand Wiersma <wijnand@videre.net>
2010-04-06 21:20:48 +0200

    Add in place filtering of servers when adding of editting a profile. Helps a lot if you need to appy it to lots of servers.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-
+  $('input#server_search').quicksearch('#server_list ol li');
 });

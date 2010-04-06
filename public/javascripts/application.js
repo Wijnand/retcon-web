b4c41882a4cd58085e2f4ba353d4142aaccff67d
Wijnand Wiersma <wijnand@videre.net>
2010-04-06 23:03:17 +0200

    Introduce sortable tables.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,5 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
   $('input#server_search').quicksearch('#server_list ol li');
+  $(".sortable").tablesorter();
 });

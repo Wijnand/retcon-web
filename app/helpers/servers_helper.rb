385a15db2f89446782d853bfd64919279b3fc575
Wijnand Wiersma <wijnand@videre.net>
2010-04-08 22:31:41 +0200

    try to do a better job regarding duration displaying.

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -3,6 +3,14 @@ module ServersHelper
   def display_backup_duration(job)
     return 'Unknown' unless job
     return 'Not yet started' if job.status == 'queued'
-    distance_of_time_in_words(job.created_at, job.updated_at )
+    start_time = job.created_at
+    end_time = job.updated_at
+    
+    if job.status == 'running'
+      start_time = job.updated_at
+      end_time = Time.new
+    end
+      
+    distance_of_time_in_words(start_time, end_time )
   end
 end

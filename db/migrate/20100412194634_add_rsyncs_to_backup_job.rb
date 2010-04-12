d13a697cb8a2bc2c28487b30e96000291948e739
Wijnand Wiersma <wijnand@videre.net>
2010-04-12 21:57:26 +0200

    Rename rsyncs to stored_rsyncs to simplify code

--- db/migrate/20100412194634_add_rsyncs_to_backup_job.rb
+++ db/migrate/20100412194634_add_rsyncs_to_backup_job.rb
@@ -1,9 +1,9 @@
 class AddRsyncsToBackupJob < ActiveRecord::Migration
   def self.up
-    add_column :backup_jobs, :rsyncs, :text
+    add_column :backup_jobs, :stored_rsyncs, :text
   end
 
   def self.down
-    remove_column :backup_jobs, :rsyncs
+    remove_column :backup_jobs, :stored_rsyncs
   end
 end

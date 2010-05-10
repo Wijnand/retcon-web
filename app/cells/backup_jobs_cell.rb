06b012bef220ddfc120a9b06743794a69ed14646
Wijnand Wiersma <wijnand@videre.net>
2010-05-10 08:43:52 +0200

    fix n+1 query

--- app/cells/backup_jobs_cell.rb
+++ app/cells/backup_jobs_cell.rb
@@ -1,18 +1,18 @@
 class BackupJobsCell < Cell::Base
   def running
-    @backup_jobs = BackupJob.running
+    @backup_jobs = BackupJob.running(:include => [:servers])
     render
   end
   
   def problems
-    @backup_jobs = BackupJob.latest_problems.select do | job |
+    @backup_jobs = BackupJob.latest_problems(:include => [:servers]).select do | job |
       job.server.backup_jobs.last == job
     end
     render
   end
   
   def queued
-    @backup_jobs = BackupJob.queued
+    @backup_jobs = BackupJob.queued(:include => [:servers])
     render
   end
 end

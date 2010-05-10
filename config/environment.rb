17182845d1f965ca9d9ddb1488591cfc61d2c52c
Wijnand Wiersma <wijnand@videre.net>
2010-05-10 08:19:54 +0200

    Fix the will_paginate version to 2.3.12 for now.

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "formtastic"
   config.gem "declarative_authorization"
   config.gem "searchlogic"
-  config.gem "will_paginate"
+  config.gem "will_paginate", :version => '2.3.12'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

f273b72a134b16f86978af5f31b077b637140d34
Wijnand Wiersma <wijnand@videre.net>
2010-07-07 18:45:36 +0200

    Move to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,18 @@ module ApplicationHelper
   def selected_tab?(cont)
     @controller.controller_name == cont ? 'active' : 'inactive'
   end
+  
+  def display_backup_duration(job)
+    return 'Unknown' unless job
+    return 'Not yet started' if job.status == 'queued'
+    start_time = job.started || job.created_at
+    end_time = job.updated_at
+    
+    if job.status == 'running'
+      start_time = job.updated_at
+      end_time = Time.new
+    end
+      
+    distance_of_time_in_words(start_time, end_time )
+  end
 end

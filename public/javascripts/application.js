85ea7004ba06344f523b493468008a4c4014c396
Wijnand Wiersma <wijnand@videre.net>
2010-07-08 18:50:54 +0200

    new way to display commands

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,18 +12,7 @@ $(document).ready(function() {
     });
   });
   
-});
-TopUp.images_path = "/images/top_up/";
-TopUp.players_path = "/players/";
-TopUp.addPresets({
-  ".commandstable tbody tr td a": {
-    group: "commands",
-    type: "ajax",
-    layout: "quicklook",
-    title: "command",
-    x: "0",
-    y: "0",
-    effect: "flip",
-    shaded: 1
-  }
+  $(".toggle").live('click', function(){
+    $(this).find('.togglable').toggle();
+  });
 });

b24e01b08f317d0d5dc6ace003bd08275b780624
Wijnand Wiersma <wijnand@videre.net>
2011-12-11 11:47:40 +0100

    Update to rails 2.3.10

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,10 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
+  config.gem "authlogic", :version => '2.1.3'
   config.gem "formtastic", :version => '0.9.7'
-  config.gem "cancan"
-  config.gem "searchlogic"
+  config.gem "cancan", :version => '1.3.4'
+  config.gem "searchlogic", :version => '2.4.14'
   config.gem "will_paginate", :version => '2.3.12'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
